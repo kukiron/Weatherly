6791b1ab91020411d5f7521a4103c8709185d897
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-10-28 18:18:51 +0600

    Add comments

--- js/Raven.js
+++ js/Raven.js
@@ -1,3 +1,7 @@
+/**
+ * Error tracking enabled with Sentry
+ *
+ */
 const sentry_key = 'd86963bdc35f4b5c83cc38844fe43c06';
 const sentry_app = '236902';
 const sentry_url = `https://${sentry_key}@sentry.io/${sentry_app}`;
